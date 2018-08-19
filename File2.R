0f3545967495c23580299c940b8a625cb86aee0e
suraj1801187 <suraj.p18187.com>
2018-08-19 19:52:06 +0530

    19/08

---
+++ File2.R
@@ -0,0 +1,11 @@
+mtcars
+git config --global user.email "suraj.p18187.com"
+git config --global user.name "suraj1801187"
+plot(women)
+install.packages('car')
+?women
+?mean
+x = 1:100
+x
+y = seq(1,100, by=2)
+y
