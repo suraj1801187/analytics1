0c5d0597353369c1cebbb42164171a0ba01afeea
suraj1801187 <suraj.p18187.com>
2018-08-21 18:32:16 +0530

    asd

---
+++ carseats.R
@@ -0,0 +1,36 @@
+# Decision tree
+install.packages("ISLR")
+library(ISLR)
+
+# Decision Trees : 
+
+library(ISLR)
+data(Carseats)
+head(Carseats)
+names(Carseats)
+?Carseats
+data = Carseats
+
+#Libraries for Decision Tree
+library(rpart)
+library(rpart.plot)
+
+#Model
+decisiontree = rpart(Sales ~ . , data=data, method='anova' )
+decisiontree
+rpart.plot(decisiontree)
+
+#this is large tree, so prune it: check cp
+printcp(decisiontree)
+prunetree = prune(decisiontree, cp=0.05)
+prunetree
+rpart.plot(prunetree)
+?prune
+printcp(decisiontree)
+
+#improve the plot
+rpart.plot(prunetree, nn=T, cex=.8, type=4)
+
+#https://www.datacamp.com/community/tutorials/decision-trees-R
+
+
